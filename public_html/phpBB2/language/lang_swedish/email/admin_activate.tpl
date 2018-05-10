4f16c5c877ebd186b73f38d72481d87ef8bed1aa
Olle Törnström <olle@studiomediatech.com>
2018-05-10 23:05:12 +0200

    Add translations

---
+++ public_html/phpBB2/language/lang_swedish/email/admin_activate.tpl
@@ -0,0 +1,26 @@
+Subject: Nytt användarkonto
+Charset: iso-8859-1
+
+Ny användare registrerad men inaktiverad:
+
+Användarnamn:	{USERNAME}
+E-post:			{U_EMAIL}
+Från:			{U_FROM}
+Hemsida:			{U_WEBSITE}
+Yrke:			{U_OCCUPATION}
+Intressen:			{U_INTERESTS}
+MSN:			{U_MSN}
+ICQ:			{U_ICQ}
+AIM:				{U_AIM}
+YIM:				{U_YIM}
+
+Signatur: {U_SIG}
+======================================================================
+
+För att godkänna användaren klicka på länken här:
+{U_ACTIVATE}
+
+För att omedelbart radera användaren klicka på länken här:
+{U_DELETE}
+
+Varning! Det går ej att ångra eller återskapa en raderad användare.
---
+++ public_html/phpBB2/language/lang_swedish/lang_main_album.php
@@ -0,0 +1,156 @@
+<?php
+/***************************************************************************
+ *                          lang_main_album.php [English]
+ *                              -------------------
+ *     begin                : Sunday, February 02, 2003
+ *     copyright            : (C) 2003 Smartor
+ *     email                : smartor_xp@hotmail.com
+ *
+ *     $Id: lang_main_album.php,v 1.0.6 2003/03/05 20:12:38 ngoctu Exp $
+ *
+ ****************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+//
+// Album Index
+//
+$lang['Photo_Album'] = 'Fotolbum';
+$lang['Pics'] = 'Bilder';
+$lang['Last_Pic'] = 'Senaste bilden';
+$lang['Public_Categories'] = 'Kategorier';
+$lang['No_Pics'] = 'Inga bilder';
+$lang['Users_Personal_Galleries'] = 'Medlemsgallerier';
+$lang['Your_Personal_Gallery'] = 'Ditt personliga galleri';
+$lang['Recent_Public_Pics'] = 'Senaste bilderna';
+
+$lang['View'] = 'Visad';
+
+//
+// Category View
+//
+$lang['Category_not_exist'] = 'Den här kategorin finns ej';
+$lang['Upload_Pic'] = 'Ladda upp en bild';
+$lang['Pic_Title'] = 'Titel';
+
+$lang['Album_upload_can'] = 'You <b>can</b> upload new pics in this category';
+$lang['Album_upload_cannot'] = 'You <b>cannot</b> upload new pics in this category';
+$lang['Album_rate_can'] = 'You <b>can</b> rate pics in this category';
+$lang['Album_rate_cannot'] = 'You <b>cannot</b> rate pics in this category';
+$lang['Album_comment_can'] = 'You <b>can</b> post comments to pics in this category';
+$lang['Album_comment_cannot'] = 'You <b>cannot</b> post comments to pics in this category';
+$lang['Album_edit_can'] = 'You <b>can</b> edit your pics and comments in this category';
+$lang['Album_edit_cannot'] = 'You <b>cannot</b> edit your pics and comments in this category';
+$lang['Album_delete_can'] = 'You <b>can</b> delete your pics and comments in this category';
+$lang['Album_delete_cannot'] = 'You <b>cannot</b> delete your pics and comments in this category';
+$lang['Album_moderate_can'] = 'You <b>can</b> %smoderate%s this category';
+
+$lang['Edit_pic'] = 'Ändra';
+$lang['Delete_pic'] = 'Radera';
+$lang['Rating'] = 'Betyg';
+$lang['Comments'] = 'Kommentarer';
+$lang['New_Comment'] = 'Inga kommentarer';
+
+$lang['Not_rated'] = '<i>inget betyg</i>';
+
+//
+// Upload
+//
+$lang['Pic_Desc'] = 'Beskrivning';
+$lang['Plain_text_only'] = 'Bara vanliga text';
+$lang['Max_length'] = 'Max längd (tecken)';
+$lang['Upload_pic_from_machine'] = 'Ladda upp en bild från din dator';
+$lang['Upload_to_Category'] = 'Ladda upp till kategori';
+$lang['Upload_thumbnail_from_machine'] = 'Ladda upp tumnagelbild från din dator (måste vara samma typ som din bild)';
+$lang['Upload_thumbnail'] = 'Ladda upp en tumnagelbild';
+$lang['Upload_thumbnail_explain'] = 'Den måste vara av samma typ som din bild';
+$lang['Thumbnail_size'] = 'Tumnagelbildens storlek (pixlar)';
+$lang['Filetype_and_thumbtype_do_not_match'] = 'Din bild och din tumnagelbild måste vara samma typ';
+
+$lang['Upload_no_title'] = 'Du måste ange en titel för din bild';
+$lang['Upload_no_file'] = 'Du måste ange sökvägen till din fil';
+$lang['Desc_too_long'] = 'Din beskrivning är för lång';
+
+$lang['Max_file_size'] = 'Största filstorlek (bytes)';
+$lang['Max_width'] = 'Största bildbredd (pixlar)';
+$lang['Max_height'] = 'Största bildhöjd (pixlar)';
+
+$lang['JPG_allowed'] = 'Du kan ladda upp JPG filer';
+$lang['PNG_allowed'] = 'Du kan ladda upp PNG filer';
+$lang['GIF_allowed'] = 'Du kan ladda upp GIF filer';
+
+$lang['Album_reached_quota'] = 'Den här kategorin har fyllt sin quota med bilder. Du kan inte ladda upp mer bilder. Kontakta moderatorn för mer information.';
+$lang['User_reached_pics_quota'] = 'Du har fyllt din quota med bilder. Du kan inte ladd upp mer bilder. Kontakta moderatorn för mer information.';
+
+$lang['Bad_upload_file_size'] = 'Din bild är för stor eller trasig';
+$lang['Not_allowed_file_type'] = 'Filtypen är inte tillåtet';
+$lang['Upload_image_size_too_big'] = 'Din bild är för stor i höjd eller bredd';
+$lang['Upload_thumbnail_size_too_big'] = 'Din tumnagelbild är för stor i höjd eller bredd';
+
+$lang['Missed_pic_title'] = 'Du måste ange en titel på bilden';
+
+$lang['Album_upload_successful'] = 'Din bild är uppladdad';
+$lang['Album_upload_need_approval'] = 'Din bild är uppladdad.<br /><br />Men godkännande är påslaget så din bild måste först godkännas av en moderator innan den kan visas';
+$lang['Click_return_category'] = 'Klicka %shär%s för att gå tillbaka till kategorin';
+$lang['Click_return_album_index'] = 'Klicka %shär%s för att gå tillbaka till Bilder';
+
+// View Pic
+$lang['Pic_not_exist'] = 'Bilden finns ej';
+
+// Edit Pic
+$lang['Edit_Pic_Info'] = 'Redigera bildinformation';
+$lang['Pics_updated_successfully'] = 'Bildinformationen har blivit uppdaterad';
+
+// Delete Pic
+$lang['Album_delete_confirm'] = 'Är du säker på att du vill radera bilden/bilderna?';
+$lang['Pics_deleted_successfully'] = 'Bilder/bilderna har blivit raderade';
+
+//
+// ModCP
+//
+$lang['Approval'] = 'Approval';
+$lang['Approve'] = 'Approve';
+$lang['Unapprove'] = 'Unapprove';
+$lang['Status'] = 'Status';
+$lang['Locked'] = 'Låst';
+$lang['Not_approved'] = 'Not approved';
+$lang['Approved'] = 'Approved';
+$lang['Move_to_Category'] = 'Flytta till kategori';
+$lang['Pics_moved_successfully'] = 'Your pic(s) have been moved successfully';
+$lang['Pics_locked_successfully'] = 'Your pic(s) have been locked successfully';
+$lang['Pics_unlocked_successfully'] = 'Your pic(s) have been unlocked successfully';
+$lang['Pics_approved_successfully'] = 'Your pic(s) have been approved successfully';
+$lang['Pics_unapproved_successfully'] = 'Your pic(s) have been unapproved successfully';
+
+//
+// Rate
+//
+$lang['Current_Rating'] = 'Betyg';
+$lang['Please_Rate_It'] = 'Betygsätt bilden';
+$lang['Already_rated'] = 'Du har redan betygsatt den här bilden';
+$lang['Album_rate_successfully'] = 'Bilden har blivit betygsatt';
+
+//
+// Comment
+//
+$lang['Comment_no_text'] = 'Skriv din kommentar';
+$lang['Comment_too_long'] = 'Din kommentar är för lång';
+$lang['Comment_delete_confirm'] = 'Är du säker på att du vill radera den här kommentaren?';
+$lang['Pic_Locked'] = 'Tyvärr, den här bilden är låst. Du kan inte posta några fler kommentarer.';
+
+//
+// Personal Gallery
+//
+$lang['Personal_Gallery_Explain'] = 'You can view the personal galleries of other members by clicking on the link in their profiles';
+$lang['Personal_gallery_not_created'] = 'Bildgalleriet tillhörande %s är tomt.';
+$lang['Not_allowed_to_create_personal_gallery'] = 'Tyvärr, du har ej rättigheter att skapa ett personligt galleri';
+$lang['Click_return_personal_gallery'] = 'Klicka %shär%s för att komma tillbaka till det personliga galleriet';
+
+?>
